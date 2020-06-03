b967a23dd2b1cba2f1a7ef68b3d267c35abce08d
hussainferoz <hussainferoz839@gmail.com>
2020-06-03 22:30:28 +0500

    added socket listners and driver marker animate to coordinate

--- src/screens/Map/Passenger/Index.js
+++ src/screens/Map/Passenger/Index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react';
-import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+import MapView, { PROVIDER_GOOGLE, AnimatedRegion, MarkerAnimated } from 'react-native-maps';
 
 import * as Permissions from 'expo-permissions';
 
@@ -11,21 +11,36 @@ import CustomMarker from '../CustomMarker';
 import { getSocketConnection } from '../Socket';
 import { LATITUDE_DELTA, LONGITUDE_DELTA } from '../../../Constants';
 
+const deltas = {
+	latitudeDelta: LATITUDE_DELTA,
+	longitudeDelta: LONGITUDE_DELTA
+};
+
 let socket = null;
 
 const Index = () => {
-	const { user: { passengerPoint: { location: { coordinates } } } } = useSelector((state) => state.user);
+	const { user: { passengerPoint: { location: { coordinates } }, driverId } } = useSelector((state) => state.user);
 	const [ mapMargin, setMapMargin ] = useState(1);
+	const [ rideStatus, setRideStatus ] = useState(null);
+	const [ MarkerAnimate ] = useState(
+		new AnimatedRegion({
+			latitude: 0,
+			longitude: 0,
+			...deltas
+		})
+	);
 	const mapViewRef = useRef(null);
 
 	const point = {
-		latitude: coordinates[1],
-		longitude: coordinates[0]
+		latitude: 37.4850453,
+		longitude: -122.1489067
+		// latitude: coordinates[1],
+		// longitude: coordinates[0]
 	};
 
 	useEffect(() => {
-		requestPermission();
 		socket = getSocketConnection();
+		requestPermission();
 
 		return () => {
 			console.log('Unmount');
@@ -33,18 +48,58 @@ const Index = () => {
 		};
 	}, []);
 
+	useEffect(
+		() => {
+			if (rideStatus === false) {
+				mapViewRef.current.animateToRegion(
+					{
+						...point,
+						...deltas
+					},
+					2000
+				);
+			}
+		},
+		[ rideStatus ]
+	);
+
 	const requestPermission = async () => {
 		const { status } = await Permissions.askAsync(Permissions.LOCATION);
 		if (status === Permissions.PermissionStatus.GRANTED) {
+			socketListners();
+		}
+	};
+
+	const socketListners = () => {
+		socket.emit('getRide', { id: driverId });
+
+		socket.once('getRide', getRideCallBack);
+
+		socket.on('rideStatus-' + driverId, rideStatusCallBack);
+	};
+
+	//callback function for the listner get ride.
+	const getRideCallBack = (data) => {
+		const { status, coordinates: [ longitude, latitude ] } = data;
+		setRideStatus(status);
+
+		if (status) {
+			MarkerAnimate.timing({ latitude, longitude }).start();
+
 			mapViewRef.current.animateToRegion(
 				{
-					...point,
-					latitudeDelta: LATITUDE_DELTA,
-					longitudeDelta: LONGITUDE_DELTA
+					latitude,
+					longitude,
+					...deltas
 				},
 				2000
 			);
 		}
+	};
+
+	//callback function for the listner ride status.
+	const rideStatusCallBack = (data) => {
+		setRideStatus(data.rideStatus);
 	};
 
 	return (
@@ -59,6 +114,7 @@ const Index = () => {
 				}}
 			>
 				<CustomMarker point={point} />
+				{rideStatus && <MarkerAnimated coordinate={MarkerAnimate} />}
 			</MapView>
 		</View>
 	);
