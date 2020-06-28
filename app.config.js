73cec421221db7ea2b78546e48bce9e5c006a188
hussainferoz <hussainferoz839@gmail.com>
2020-06-29 01:32:09 +0500

    apk config

--- app.config.js
+++ app.config.js
@@ -4,6 +4,8 @@ export default ({ config }) => {
 	return {
 		...config,
 		android: {
+			package: 'com.VanOn.Van_On',
+			versionCode: 1,
 			config: {
 				googleMaps: {
 					apiKey: GOOGLE_MAPS_API_KEY
